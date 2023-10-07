2998fd2f09f52c86cba023c670ab57929bba04e5
Shubh <shubhjhawar78@gmail.com>
2023-10-06 21:04:39 -0400

    major setup complete

--- components/RightSideBar.jsx
+++ components/RightSideBar.jsx
@@ -1,18 +1,32 @@
 "use client";
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 import { styles } from '@/styles';
 
+import { AiOutlineDown } from "react-icons/ai";
+
 const RightSideBar = () => {
   const pathname = usePathname()  
 
+  const [isAcademicsClicked, setIsAcademicsClicked] = useState(false);
+  const [isActivitiesClicked, setIsActivitiesClicked] = useState(false);
+
+  const handleAcademicsMouseClick = () => {
+    setIsAcademicsClicked((prev) => !prev);
+  };
+  
+  const handleActivitiesMouseClick = () => {
+    setIsActivitiesClicked((prev) => !prev);
+  };
+  
+
   const shouldUnderlineLink = (href) => {
-    return pathname === href;
+    return pathname.startsWith(href);
   };
 
   return (
-    <div  className="bg-red-500 h-full p-10 flex flex-col text-white max-md:block backdrop-blur-lg bg-opacity-75">
+    <div  className="lg:hidden bg-red-500 h-full p-10 flex flex-col text-white max-md:block backdrop-blur-lg bg-opacity-75">
       <Link href="/admissions">
         <h1 className={`${styles.rightbuttons} ${shouldUnderlineLink('/admissions') && 'text-black underline'}`}>Admissions</h1>
       </Link>
@@ -22,12 +36,57 @@ const RightSideBar = () => {
       <Link href="/courses">
         <h1 className={`${styles.rightsubbuttons} ${shouldUnderlineLink('/courses') && 'text-black underline'}`}>Courses</h1>
       </Link>
+      <Link href="/cbse">
+        <h1 className={`${styles.rightsubbuttons} ${shouldUnderlineLink('/cbse') && 'text-black underline'}`}>Cbse Curriculum</h1>
+      </Link>
+      <button 
+        className='flex items-center gap-2'
+        type="button"
+        onClick={handleAcademicsMouseClick}
+      >
+        <h1 className={`${styles.rightsubbuttons} ${shouldUnderlineLink('/academics/') && 'text-black underline'}`}>Academics</h1>
+        <AiOutlineDown />
+      </button>
+      {isAcademicsClicked && (
+        <div className='gap-2 my-1 py-1'>
+          <Link href="/academics/syllabus">
+            <p className={`${styles.rightDropDownItem}`}>Syllabus Breakup</p>
+          </Link>
+          <Link href="/academics/fees">
+            <p className={`${styles.rightDropDownItem}`}>Fees Structure</p>
+          </Link>
+        </div>
+      )}
       <Link href="/faculty">
         <h1 className={`${styles.rightsubbuttons} ${shouldUnderlineLink('/faculty') && 'text-black underline'}`}>Faculty</h1>
       </Link>
-      <Link href="/extra">
-        <h1 className={`${styles.rightsubbuttons} ${shouldUnderlineLink('/extra') && 'text-black underline'}`}>Extra Curriculars</h1>
-      </Link>
+      <button 
+        className='flex items-center gap-2'
+        type="button"
+        onClick={handleActivitiesMouseClick}
+      >
+        <h1 className={`${styles.rightsubbuttons} ${shouldUnderlineLink('/activities/') && 'text-black underline'}`}>Activities</h1>
+        <AiOutlineDown />
+      </button>
+      {isActivitiesClicked && (
+        <div className='gap-2 my-1 py-1'>
+          <Link href="/activities/annualfunction">
+            <p className={`${styles.rightDropDownItem}`}>Annual Celebration</p>
+          </Link>
+          <Link href="/activities/cocurricular">
+            <p className={`${styles.rightDropDownItem}`}>Co Curricular</p>
+          </Link>
+          <Link href="/activities/cultural">
+            <p className={`${styles.rightDropDownItem}`}>Cultural</p>
+          </Link>
+          <Link href="/activities/extra">
+            <p className={`${styles.rightDropDownItem}`}>Extra Curricular</p>
+          </Link>
+          <Link href="/activities/awards">
+            <p className={`${styles.rightDropDownItem}`}>Awards</p>
+          </Link>
+        </div>
+      )}
     </div>
   )
 }
--- styles/index.js
+++ styles/index.js
@@ -1,9 +1,12 @@
 const styles = {
-    navbuttons: "text-lg font-semibold hover:border-b-[2px] border-red-600 hover:scale-110 transition ease-in-out",
-    rightbuttons: "text-2xl font-semibold mb-4",
-    rightsubbuttons: "text-xl mb-1",
+    navbuttons: "text-lg font-semibold hover:text-red-600 hover:scale-110 transition ease-in-out",
+    rightbuttons: "text-2xl font-semibold mb-4 hover:text-red-200",
+    rightsubbuttons: "text-xl mb-1 hover:text-red-200",
     dates: 'w-full flex justify-center text-green-600 font-bold',
     titles: 'w-3/4 text-lg max-sm:text-xs',
+    dropDownMenuContainer: 'absolute top-5 mt-2 w-[200px] bg-white border border-gray-300 rounded-md shadow-lg',
+    dropDownMenuItem: 'px-4 py-2 hover:bg-gray-100',
+    rightDropDownItem: 'hover:text-red-200',
 }
 
 export { styles };
