0c0e72c7d64b29a69b9515252f8baac1b131dc10
Shubh <shubhjhawar78@gmail.com>
2023-10-08 15:18:26 -0400

    added history title

--- app/(root)/history/page.js
+++ app/(root)/history/page.js
@@ -6,6 +6,7 @@ import { history } from '@/constants';
 const History = () => {
   return (
     <div className="h-auto">
+      <h1 className='text-white text-3xl flex justify-center font-sans tracking-wide'>History</h1>
       {history.map((element, index) => (
         <HistoryContainer key={index} title={element.title} content={element.content} index={index} />
       ))}
